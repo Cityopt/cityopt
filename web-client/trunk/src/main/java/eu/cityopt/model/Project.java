eac65a1d29af65f682d28118fc72c2fe29f48c4c
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-01-22 08:24:49 +0000

    id as default sort order for components

--- web-client/trunk/src/main/java/eu/cityopt/model/Project.java
+++ web-client/trunk/src/main/java/eu/cityopt/model/Project.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -225,6 +226,7 @@ public class Project implements java.io.Serializable {
 	}
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "project")
+	@OrderBy("componentid")
 	public Set<Component> getComponents() {
 		return this.components;
 	}
