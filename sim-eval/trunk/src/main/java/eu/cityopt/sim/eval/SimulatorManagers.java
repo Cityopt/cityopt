2663ee8a6b91380a885cc3165b74030c4b6cca2a
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-01-30 10:52:05 +0000

    Add documentation, provide list of available simulators.

--- sim-eval/trunk/src/main/java/eu/cityopt/sim/eval/SimulatorManagers.java
+++ sim-eval/trunk/src/main/java/eu/cityopt/sim/eval/SimulatorManagers.java
@@ -1,7 +1,9 @@
 package eu.cityopt.sim.eval;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Access to SimulatorManager implementations for named simulators.
@@ -12,11 +14,18 @@ public class SimulatorManagers {
     private static Map<String, SimulatorManager> simulatorManagers
         = new HashMap<String, SimulatorManager>();
 
-    public static void register(String profile, SimulatorManager manager) {
-        simulatorManagers.put(profile, manager);
+    /** Returns the known simulator names. */
+    public static Set<String> foo() {
+        return Collections.unmodifiableSet(simulatorManagers.keySet());
     }
 
+    /** Returns a SimulatorManager instance for access to a named simulator. */
     public static SimulatorManager get(String simulatorName) {
         return simulatorManagers.get(simulatorName);
+    }
+
+    /** Registers a new SimulatorManager instance. */
+    public static void register(String profile, SimulatorManager manager) {
+        simulatorManagers.put(profile, manager);
     }
 }
