61fa947309e7a1c47071d61ac61a3b73842ee7b4
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-02-20 15:05:30 +0000

    Add 'put' functions to MetricValues so that it can be filled from outside the class.

--- trunk/sim-eval/src/main/java/eu/cityopt/sim/eval/MetricValues.java
+++ trunk/sim-eval/src/main/java/eu/cityopt/sim/eval/MetricValues.java
@@ -1,5 +1,6 @@
 package eu.cityopt.sim.eval;
 
+import java.text.ParseException;
 import java.util.Collection;
 
 import javax.script.Bindings;
@@ -16,18 +17,32 @@ public class MetricValues implements EvaluationContext {
     private SimulationResults results;
     private BindingLayer bindingLayer;
 
+    /**
+     * Creates an empty instance, which has to be filled in manually
+     * by calling put.
+     */
+    public MetricValues(SimulationResults results)
+                    throws ScriptException, InvalidValueException {
+        this(results, null);
+    }
+
+    /**
+     * Evaluates metric values using the given collection of metric expressions.
+     */
     public MetricValues(SimulationResults results,
-            Collection<MetricExpression> metrics) throws ScriptException,
-            InvalidValueException {
+            Collection<MetricExpression> metrics)
+                    throws ScriptException, InvalidValueException {
         this.results = results;
         final Namespace namespace = results.getNamespace();
         this.bindingLayer = new BindingLayer(namespace,
                 results.getBindingLayer(),
                 name -> (name == null) ? namespace.metrics : null,
                 "metric");
-        for (MetricExpression metric : metrics) {
-            Object value = metric.evaluate(results);
-            bindingLayer.put(null, metric.getMetricName(), value);
+        if (metrics != null) {
+            for (MetricExpression metric : metrics) {
+                Object value = metric.evaluate(results);
+                bindingLayer.put(null, metric.getMetricName(), value);
+            }
         }
     }
 
@@ -40,9 +55,30 @@ public class MetricValues implements EvaluationContext {
         return bindingLayer.get(null, metricName);
     }
 
-    /** Gets the value of a named metric as a formatted string. */
+    /** Gets the value of a named metric that is a time series. */
+    public TimeSeriesI getTS(String metricName) {
+        return (TimeSeriesI) bindingLayer.get(null, metricName);
+    }
+
+    /**
+     * Gets the value of a named metric as a formatted string.
+     * Not useful for time series.
+     */
     public String getString(String metricName) {
         return bindingLayer.getString(null, metricName);
+    }
+
+    /** Sets the value of a named metric. */
+    public void put(String metricName, Object value) {
+        bindingLayer.put(null, metricName, value);
+    }
+
+    /**
+     * Parses a metric value and stores it. Not useful for time series.
+     * @throws ParseException if the string cannot be parsed as the correct type
+     */
+    public Object putString(String metricName, String value) throws ParseException {
+        return bindingLayer.putString(null, metricName, value);
     }
 
     @Override
