ad693801fa0145e324c806782b3091bbe59b1210
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-02-20 16:00:13 +0000

    Also rename the creator and add the type parameter.

--- trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/MixedCreator.java
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/MixedCreator.java
@@ -2,10 +2,10 @@ package eu.cityopt.opt.ga;
 
 import org.opt4j.core.problem.Creator;
 
-public class RandomCreator implements Creator<MixedGenotype> {
+public class MixedCreator<Key> implements Creator<MixedGenotype<Key>> {
 
     @Override
-    public MixedGenotype create() {
+    public MixedGenotype<Key> create() {
         // TODO Auto-generated method stub
         return null;
     }
