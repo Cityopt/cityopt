71433aed941fff2b9c4d4ccaf6103bc683e18017
fnadler <fnadler@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-02-20 06:44:42 +0000

    Lookups added for Types, DataReliability

---
+++ trunk/web-client/sql/datareliability.sql
@@ -0,0 +1,4 @@
+INSERT INTO "datareliability" VALUES ('Actual data');
+INSERT INTO "datareliability" VALUES ('Technical report');
+INSERT INTO "datareliability" VALUES ('Scientific bibliography');
+INSERT INTO "datareliability" VALUES ('Expert opinion');
---
+++ trunk/web-client/sql/types.sql
@@ -0,0 +1,7 @@
+INSERT INTO "type" VALUES ('Double');
+INSERT INTO "type" VALUES ('Integer');
+INSERT INTO "type" VALUES ('String');
+INSERT INTO "type" VALUES ('TimeSeries/Step');
+INSERT INTO "type" VALUES ('TimeSeries/linear');
+INSERT INTO "type" VALUES ('List of Double');
+INSERT INTO "type" VALUES ('List of Integer');
