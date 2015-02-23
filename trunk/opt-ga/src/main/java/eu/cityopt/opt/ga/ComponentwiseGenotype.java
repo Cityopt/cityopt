258d87fb4df30e8cb445b08bdf6a02ee64713439
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-02-23 17:20:31 +0000

    Add an init method because it is so easy.

--- trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/ComponentwiseGenotype.java
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/ComponentwiseGenotype.java
@@ -1,7 +1,12 @@
 package eu.cityopt.opt.ga;
 
+import org.opt4j.core.common.random.Rand;
 import org.opt4j.core.genotype.CompositeGenotype;
 
 public class ComponentwiseGenotype
 extends CompositeGenotype<String, MixedGenotype<String>> {
+    /** Initialise with random values */
+    void init(Rand rand) {
+        values().forEach(v -> v.init(rand));
+    }
 }
