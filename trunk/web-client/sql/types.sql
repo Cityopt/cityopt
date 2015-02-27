e0e61ded51cf7a6fa705ad1e56a18ffd1b0bf9e6
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-02-27 07:57:41 +0000

    Add timestamp types to database

--- trunk/web-client/sql/types.sql
+++ trunk/web-client/sql/types.sql
@@ -1,7 +1,9 @@
 INSERT INTO "type" VALUES ('Double');
 INSERT INTO "type" VALUES ('Integer');
 INSERT INTO "type" VALUES ('String');
+INSERT INTO "type" VALUES ('Timestamp');
 INSERT INTO "type" VALUES ('TimeSeries/Step');
-INSERT INTO "type" VALUES ('TimeSeries/linear');
+INSERT INTO "type" VALUES ('TimeSeries/Linear');
 INSERT INTO "type" VALUES ('List of Double');
 INSERT INTO "type" VALUES ('List of Integer');
+INSERT INTO "type" VALUES ('List of Timestamp');
