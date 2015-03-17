ee19c8013213ce4366d9bb58d0d55f43eab95706
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-03-17 08:30:55 +0000

    Better working GridSearch implementation (multi-scenario generator)

---
+++ trunk/sim-eval/src/main/java/eu/cityopt/sim/opt/ParetoFront.java
@@ -0,0 +1,43 @@
+package eu.cityopt.sim.opt;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Maintains a Pareto front, i.e. a collection of non-dominated solutions. 
+ *
+ * @author Hannu Rummukainen
+ */
+public class ParetoFront {
+    /** The non-dominated solutions. */
+    public List<Solution> solutions = new ArrayList<>();
+
+    /**
+     * Updates the collection of non-dominated solutions, given a new solution.
+     * Any existing solutions dominated by the new solution are removed. The new
+     * solution is added to the collection unless it is dominated by an existing
+     * solution.
+     * 
+     */
+    void add(Solution newSolution) {
+        if (newSolution != null && newSolution.metricValues != null
+                && newSolution.constraintStatus.isDefinitelyFeasible()) {
+            Iterator<Solution> it = solutions.iterator();
+            while (it.hasNext()) {
+                Solution oldSolution = it.next();
+                Integer cmp = oldSolution.compareTo(newSolution);
+                if (cmp != null) {
+                    if (cmp < 0) {
+                        // oldSolution dominates newSolution
+                        return;
+                    } else if (cmp > 0) {
+                        // newSolution dominates oldSolution
+                        it.remove();
+                    }
+                }
+            }
+            solutions.add(newSolution);
+        }
+    }
+}
