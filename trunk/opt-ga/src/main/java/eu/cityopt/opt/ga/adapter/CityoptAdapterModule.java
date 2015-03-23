6b1f754001d283c76a39a1ad8caf8b4c94c91ab6
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-03-23 11:24:38 +0000

    Timeout mechanism for optimisation.

--- trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/adapter/CityoptAdapterModule.java
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/adapter/CityoptAdapterModule.java
@@ -1,19 +1,29 @@
 package eu.cityopt.opt.ga.adapter;
 
+import org.opt4j.core.optimizer.Control;
+
 import com.google.inject.AbstractModule;
 
 import eu.cityopt.opt.ga.CityoptModule;
 import eu.cityopt.sim.opt.OptimisationProblem;
 import eu.cityopt.sim.eval.SimulationStorage;
 
+/**
+ * Configures the OptimisationProblem and SimulationStorage for CityoptModule.
+ *
+ * @author Hannu Rummukainen
+ */
 public class CityoptAdapterModule extends AbstractModule {
     SimulationStorage storage;
     OptimisationProblem problem;
+    Control control;
 
     public CityoptAdapterModule(
-            OptimisationProblem problem, SimulationStorage storage) {
+            OptimisationProblem problem, SimulationStorage storage,
+            Control control) {
         this.problem = problem;
         this.storage = storage;
+        this.control = control;
     }
 
     @Override
@@ -21,6 +31,9 @@ public class CityoptAdapterModule extends AbstractModule {
         install(new CityoptModule());
         bind(OptimisationProblem.class).toInstance(problem);
         bind(SimulationStorage.class).toInstance(storage);
+        if (control != null) {
+            bind(Control.class).toInstance(control);
+        }
     }
 
 }
