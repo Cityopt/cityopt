c763c66b3d945017cba59476221d07fd056d9d1c
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-03-23 10:21:36 +0000

    added ExtParamValSetService

---
+++ web-client/src/main/java/eu/cityopt/DTO/ExtParamValSetDTO.java
@@ -0,0 +1,9 @@
+package eu.cityopt.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class ExtParamValSetDTO {
+	@Getter @Setter private int extparamvalsetid;
+}
--- web-client/src/main/java/eu/cityopt/repository/ExtParamValSetCompRepository.java
+++ web-client/src/main/java/eu/cityopt/repository/ExtParamValSetCompRepository.java
@@ -1,11 +1,18 @@
 package eu.cityopt.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import eu.cityopt.model.Component;
+import eu.cityopt.model.ExtParamVal;
 import eu.cityopt.model.ExtParamValSetComp;
 
 @Repository
 public interface ExtParamValSetCompRepository  extends JpaRepository<ExtParamValSetComp, Integer>{
-
+	@Query("select epv from ExtParamValSetComp e JOIN e.extparamval as epv where extParamValSetID = :epvsid")
+	List<ExtParamVal> findByExtParamValSetId(@Param("epvsid") int epvsId);
 }
