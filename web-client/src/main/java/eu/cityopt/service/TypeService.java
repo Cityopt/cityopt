3fbcfe291949e9bb4b5473b9146b994bba84dadb
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-03-30 08:13:09 +0000

    changed TypeService to DTO

--- web-client/src/main/java/eu/cityopt/service/TypeService.java
+++ web-client/src/main/java/eu/cityopt/service/TypeService.java
@@ -1,7 +1,13 @@
 package eu.cityopt.service;
 
-import eu.cityopt.model.Type;
+import eu.cityopt.DTO.TypeDTO;
 
-public interface TypeService extends CityOptService<Type> {
+public interface TypeService extends CityOptService<TypeDTO> {
+	
+	public TypeDTO save(TypeDTO t);
+	
+	public TypeDTO update(TypeDTO toUpdate) throws EntityNotFoundException;
+
+	public TypeDTO findByID(int id) throws EntityNotFoundException;
 
 }
