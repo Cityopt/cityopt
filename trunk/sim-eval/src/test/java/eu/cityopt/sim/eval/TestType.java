5518a625b5608fc52877d4bd99c66bcd9fcf7609
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-04-01 14:12:37 +0000

    Another unused import.

--- trunk/sim-eval/src/test/java/eu/cityopt/sim/eval/TestType.java
+++ trunk/sim-eval/src/test/java/eu/cityopt/sim/eval/TestType.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 import java.text.ParseException;
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.TimeZone;
 
