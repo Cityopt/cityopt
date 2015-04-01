a7499c4da522b96c15c669e932d699230233a06e
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-04-01 09:05:58 +0000

    Document one of Hannu's changes.

--- opt-ga/src/main/java/eu/cityopt/opt/ga/CityoptModule.java
+++ opt-ga/src/main/java/eu/cityopt/opt/ga/CityoptModule.java
@@ -4,11 +4,13 @@ import org.opt4j.core.config.annotations.Ignore;
 import org.opt4j.core.problem.ProblemModule;
 
 import eu.cityopt.sim.eval.SimulationStorage;
+import eu.cityopt.sim.opt.ScenarioNameFormat;
 
 /**
  * Configure Opt4J for Cityopt.
- * To complete the problem definition bindings for {@link OptimisationProblem}
- * and optionally {@link SimulationStorage} must be added separately.
+ * To complete the problem definition a binding for {@link OptimisationProblem}
+ * must be added separately.  Bindings for {@link SimulationStorage} and
+ * {@link ScenarioNameFormat} may also be added; default is no storage.
  * To execute an optimisation task more bindings are needed, at minimum
  * an optimisation algorithm.
  * 
