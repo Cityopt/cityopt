7deb4b14031c41a1102d8aa94d09cb20e280f36b
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-04-13 13:42:08 +0000

    Determine model input parameters and output variables from the model zip file. Apros outputs are found if there are sample result files in the zip.

--- trunk/sim-eval/src/main/java/eu/cityopt/sim/eval/SimulationModel.java
+++ trunk/sim-eval/src/main/java/eu/cityopt/sim/eval/SimulationModel.java
@@ -1,6 +1,7 @@
 package eu.cityopt.sim.eval;
 
 import java.io.Closeable;
+import java.io.IOException;
 import java.time.Instant;
 
 import org.w3c.dom.Document;
@@ -8,7 +9,6 @@ import org.w3c.dom.Document;
 /**
  * Generic simulator-agnostic handle to a simulation model.
  *
- * TODO: generic access to model structure for import
  * @author Hannu Rummukainen
  */
 public interface SimulationModel extends Closeable {
@@ -17,6 +17,22 @@ public interface SimulationModel extends Closeable {
 
     /** Returns origin of simulation time, or null if not known. */
     Instant getTimeOrigin();
+
+    /**
+     * Determines the available model input parameters and output variables
+     * as far as possible.  The results may have to be completed manually.
+     *
+     * @param newNamespace an empty Namespace to which new components,
+     *   inputs and outputs will be created.  You will probably also need
+     *   to call {@link Namespace#initConfigComponent()}
+     * @param detailLevel indicates how much of the available input
+     *   parameters and output variables are to be included.  0 is minimal,
+     *   larger numbers may provide more results.
+     * @return human-readable warning messages about possible problems,
+     *   e.g. if there are invalid component or variable names.
+     */
+    String findInputsAndOutputs(
+            Namespace newNamespace, int detailLevel) throws IOException;
 
     /** Access to Apros user component structure, or null if not available. */
     Document getAprosUserComponentStructure();
---
+++ trunk/sim-eval/src/test/java/eu/cityopt/sim/eval/apros/AprosTestBase.java
@@ -0,0 +1,30 @@
+package eu.cityopt.sim.eval.apros;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+
+public class AprosTestBase {
+    private final static String propsName = "/apros/test.properties";
+    protected static Properties props;
+    protected static Path dataDir;
+    protected static Path profileDir;
+    protected static String profileName;
+
+    @BeforeClass
+    public static void setupProps() throws Exception {
+        URL purl = AprosRunnerTest.class.getResource(propsName);
+        props = new Properties();
+        try (InputStream str = purl.openStream()) {
+            props.load(str);
+        }
+        dataDir = Paths.get(purl.toURI()).getParent();
+        profileDir = dataDir.resolve(
+                props.getProperty("profile_dir"));
+        profileName = props.getProperty("profile");
+    }
+}
