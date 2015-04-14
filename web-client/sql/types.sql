8916be2074bb2f78b33d6d3477c286a6be9ee6c8
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-04-14 13:24:37 +0000

    Changes types.sql into a working insert statement.

--- web-client/sql/types.sql
+++ web-client/sql/types.sql
@@ -1,9 +1,10 @@
-INSERT INTO "type" VALUES ('Double');
-INSERT INTO "type" VALUES ('Integer');
-INSERT INTO "type" VALUES ('String');
-INSERT INTO "type" VALUES ('Timestamp');
-INSERT INTO "type" VALUES ('TimeSeries/Step');
-INSERT INTO "type" VALUES ('TimeSeries/Linear');
-INSERT INTO "type" VALUES ('List of Double');
-INSERT INTO "type" VALUES ('List of Integer');
-INSERT INTO "type" VALUES ('List of Timestamp');
+INSERT INTO Type (typeID, name) VALUES
+ (1, 'Double'),
+ (2, 'Integer'),
+ (3, 'String'),
+ (4, 'Timestamp'),
+ (5, 'TimeSeries/step'),
+ (6, 'TimeSeries/linear'),
+ (7, 'List of Double'),
+ (8, 'List of Integer'),
+ (9, 'List of Timestamp');
