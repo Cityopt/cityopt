30bb743a57f6c1117cf567cfe59516e4716fcc43
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-04-23 14:14:01 +0000

    commented in projectDTO

--- web-client/src/main/java/eu/cityopt/DTO/MetricDTO.java
+++ web-client/src/main/java/eu/cityopt/DTO/MetricDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 public class MetricDTO {
 	@Getter @Setter private int metid;
 //	@Getter @Setter private Unit unit;
-//	@Getter @Setter private ProjectDTO project;
+	@Getter @Setter private ProjectDTO project;
 	@Getter @Setter private String name;
 	@Getter @Setter private String expression;
 //	@Getter @Setter private Set<MetricVal> metricvals = new HashSet<MetricVal>(0);
