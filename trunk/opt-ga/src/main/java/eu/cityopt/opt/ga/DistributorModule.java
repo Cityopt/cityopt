4172521d3112a554aa3bc8c672004afa120205b9
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-05-06 12:25:48 +0000

    Opt4J module for distributed computing configuration.
    This is currently rather broken in sim-eval, but works when
    there is only one AprosRunner.

---
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/DistributorModule.java
@@ -0,0 +1,38 @@
+package eu.cityopt.opt.ga;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.opt4j.core.config.Icons;
+import org.opt4j.core.config.annotations.File;
+import org.opt4j.core.config.annotations.Icon;
+import org.opt4j.core.config.annotations.Info;
+import org.opt4j.core.start.Constant;
+import org.opt4j.core.start.Opt4JModule;
+
+import com.google.inject.name.Names;
+
+@Icon(Icons.PUZZLE_BLUE)
+@Info("Configure job distribution")
+public class DistributorModule extends Opt4JModule {
+    @Info("Distributed node configuration")
+    @Constant(value="nodeFile", namespace=DistributorModule.class)
+    @File(".json")
+    private String nodeFile = "";
+
+    @Override
+    protected void config() {
+        if (!nodeFile.isEmpty()) {
+            bind(Path.class).annotatedWith(Names.named("nodeConfig"))
+                    .toInstance(Paths.get(nodeFile));
+        }
+    }
+
+    public String getNodeFile() {
+        return nodeFile;
+    }
+
+    public void setNodeFile(String nodeFile) {
+        this.nodeFile = nodeFile;
+    }
+}
--- trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/SimulatorProvider.java
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/SimulatorProvider.java
@@ -1,18 +1,25 @@
 package eu.cityopt.opt.ga;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 
 import org.opt4j.core.start.Constant;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.name.Named;
 
 import eu.cityopt.sim.eval.ConfigurationException;
 import eu.cityopt.sim.eval.SimulatorManager;
 import eu.cityopt.sim.eval.SimulatorManagers;
 import eu.cityopt.sim.eval.apros.AprosManager;
+import eu.cityopt.sim.eval.apros.AprosRunner;
 
 /**
  * A Provider wrapper around {@link SimulatorManagers}.
@@ -44,5 +51,17 @@ public class SimulatorProvider implements Provider<SimulatorManager> {
         AprosManager.register(
                 Paths.get(aprosDir), Executors.newCachedThreadPool());
         manager = SimulatorManagers.get(simulator);
+    }
+    
+    /**
+     * Sets the simulator node configuration.
+     * @param config node configuration file containing a JSON object array
+     */
+    @Inject(optional=true)
+    public void setNodeConfig(@Named("nodeConfig") Path config)
+            throws IOException {
+        AprosRunner.nodes = (new ObjectMapper()).readValue(
+                config.toFile(),
+                new TypeReference<List<Map<String, String>>>() {});
     }
 }
