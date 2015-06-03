fdcdca30befdcc11df993750ea32886b3d61e1a4
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-06-03 12:41:15 +0000

    test fix

--- web-client/src/test/java/eu/cityopt/service/CopyServiceTest.java
+++ web-client/src/test/java/eu/cityopt/service/CopyServiceTest.java
@@ -3,6 +3,12 @@ package eu.cityopt.service;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,8 +25,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
-import eu.cityopt.repository.CustomQueryRepository;
+import eu.cityopt.DTO.ComponentDTO;
+import eu.cityopt.DTO.InputParameterDTO;
+import eu.cityopt.DTO.ProjectDTO;
+import eu.cityopt.repository.CustomQueryRepositoryImpl;
+import eu.cityopt.repository.ProjectRepository;
 import eu.cityopt.repository.ScenarioRepository;
+import eu.cityopt.sim.service.SimulationService;
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,16 +42,43 @@ import eu.cityopt.repository.ScenarioRepository;
     DbUnitTestExecutionListener.class })
 public class CopyServiceTest {
 	
-	@Autowired CopyService copyService;
-	@Autowired ProjectService projectService;
-	@Autowired ScenarioService scenarioService;
 	@Autowired ScenarioRepository scenarioRepository;
-	@Autowired CustomQueryRepository customQueryRepository;
+	@Autowired CustomQueryRepositoryImpl customQueryRepository;
+	
+	@Autowired ProjectService projectService; 	
+	@Autowired ScenarioService scenarioService;	
+	@Autowired AppUserService userService;	
+	@Autowired ComponentService componentService;
+	@Autowired ComponentInputParamDTOService componentInputParamService;	
+	@Autowired InputParameterService inputParamService;
+	@Autowired InputParamValService inputParamValService;
+	@Autowired ExtParamService extParamService;
+	@Autowired ExtParamValService extParamValService;
+	@Autowired ExtParamValSetService extParamValSetService;	
+	@Autowired MetricService metricService;
+	@Autowired MetricValService metricValService;
+	@Autowired UnitService unitService;	
+	@Autowired SimulationService simService;
+	@Autowired SimulationResultService simResultService;
+	@Autowired TimeSeriesService timeSeriesService;
+	@Autowired OutputVariableService outputVarService;	
+	@Autowired TypeService typeService;
+	@Autowired CopyService copyService;	
+	@Autowired OptimizationSetService optSetService;	
+	@Autowired ObjectiveFunctionService objFuncService;	
+	@Autowired OptConstraintService optConstraintService;	
+	@Autowired OptSearchConstService optSearchService;	
+	@Autowired ScenarioGeneratorService scenGenService;	
+	@Autowired DecisionVariableService decisionVarService;	
+	
+	@PersistenceContext
+	private EntityManager em;
 	
 	
 	@Test
 //	@Rollback(false)
-	@DatabaseSetup({"classpath:/testData/globalTestData.xml", "classpath:/testData/project1TestData.xml"})
+	@DatabaseSetup({"classpath:/testData/globalTestData.xml", "classpath:/testData/project1TestData.xml",
+	 "classpath:/testData/Sample Test case - SC1.xml"})
 	public void copyProject() throws EntityNotFoundException{
 
 		try {
@@ -50,7 +88,24 @@ public class CopyServiceTest {
 			e.printStackTrace();
 		}
 		
-		assertEquals(projectService.findByName("copy of").size(),1);
+		em.clear();
+		
+		List<ProjectDTO> pList = projectService.findByNameContaining("copy of");
+		
+		assertEquals(1, pList.size());
+		ProjectDTO p = pList.get(0);
+		
+		List<ComponentDTO> compList = projectService.getComponents(p.getPrjid()); 
+		assertEquals(3, compList.size());
+		ComponentDTO[] comps = compList.stream().filter(c -> c.getName().equals("Solar_thermal_panels")).toArray(size -> new ComponentDTO[size]);
+		assertNotNull(comps);
+		Set<InputParameterDTO> ipSet = componentService.getInputParameters(comps[0].getComponentid());
+		assertEquals(1, ipSet.stream().filter(i -> i.getName().equals("collector_area")).count());
+		assertEquals(1, ipSet.stream().filter(i -> i.getName().equals("Heat_loss_coefficient")).count());
+		assertEquals(1, ipSet.stream().filter(i -> i.getName().equals("Temperature_dependence_of_the_heat_losses")).count());
+		assertEquals(1, ipSet.stream().filter(i -> i.getName().equals("zero_loss_coefficient_for_total_or_global_radiation_at_normal_incidence")).count());
+		
+	
 	}
 	
 	@Test
@@ -65,7 +120,7 @@ public class CopyServiceTest {
 			e.printStackTrace();
 		}
 		
-		assertEquals(projectService.findByName("copy of").size(),1);
+		assertEquals(projectService.findByNameContaining("copy of").size(),1);
 	}	
 	
 	@Test
