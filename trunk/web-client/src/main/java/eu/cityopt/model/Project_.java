dd81f8089a180b1b38d57bc99f118cd226ac327f
mmayerhofer <mmayerhofer@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-06-04 17:14:59 +0000

    deleted Project metamodel

--- trunk/web-client/src/main/java/eu/cityopt/model/Project_.java
+++
@@ -1,9 +0,0 @@
-package eu.cityopt.model;
-
-import javax.persistence.metamodel.SingularAttribute;
-import javax.persistence.metamodel.StaticMetamodel;
-
-@StaticMetamodel(Project.class)
-public class Project_ {
-	public static volatile SingularAttribute<Project, String> name;
-}
