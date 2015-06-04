8da4c53719d9d2c1b1000da6073c8e908d04f01d
ostenlund <ostenlund@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-06-04 12:19:52 +0000

    Fixed time series bug.

--- web-client/src/main/java/eu/cityopt/DTO/TimeSeriesDTO.java
+++ web-client/src/main/java/eu/cityopt/DTO/TimeSeriesDTO.java
@@ -11,7 +11,7 @@ public class TimeSeriesDTO extends BaseDTO {
 
 	@Getter @Setter private int tseriesid;
 	@Getter @Setter private TypeDTO type;
-	@Getter @Setter private Set<TimeSeriesVal> timeseriesvals = new HashSet<TimeSeriesVal>(0);
+//	@Getter @Setter private Set<TimeSeriesVal> timeseriesvals = new HashSet<TimeSeriesVal>(0);
 //	@Getter @Setter private Set<MetricVal> metricvals = new HashSet<MetricVal>(0);
 //	@Getter @Setter private Set<ExtParamVal> extparamvals = new HashSet<ExtParamVal>(0);
 
