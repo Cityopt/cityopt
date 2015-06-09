a2d44f370d21ee8d24e161b730323fc64907fb35
hrummukainen <hrummukainen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-06-09 13:37:12 +0000

    Update simulation test data to refer by id to fixed type, algorithm and algoparam rows in the database.

--- web-client/sql/CityOPT.sql
+++ web-client/sql/CityOPT.sql
@@ -1539,18 +1539,18 @@ ALTER TABLE UserGroupProject ADD CONSTRAINT FK_UserGroupProject_UserGroups
 INSERT INTO Algorithm (algorithmId, description) VALUES
  (1, 'grid search'),
  (2, 'genetic algorithm');
-INSERT INTO AlgoParam (algorithmID, name, defaultValue) VALUES
- (1, 'max runtime [minutes]', '1000000'),
- (1, 'max parallel evaluations', '100'),
- (1, 'max scenarios', '10000'),
- (2, 'max runtime [minutes]', '1000000'),
- (2, 'max parallel evaluations', '100'),
- (2, 'seed of the random number generator', '1'),
- (2, 'number of generations', '10'),
- (2, 'population size', '100'),
- (2, 'number of parents per generation', '25'),
- (2, 'number of offspring per generation', '25'),
- (2, 'crossover rate', '0.95');
+INSERT INTO AlgoParam (aParamsID, algorithmID, name, defaultValue) VALUES
+ (1, 1, 'max runtime [minutes]', '1000000'),
+ (2, 1, 'max parallel evaluations', '100'),
+ (3, 1, 'max scenarios', '10000'),
+ (4, 2, 'max runtime [minutes]', '1000000'),
+ (5, 2, 'max parallel evaluations', '100'),
+ (6, 2, 'seed of the random number generator', '1'),
+ (7, 2, 'number of generations', '10'),
+ (8, 2, 'population size', '100'),
+ (9, 2, 'number of parents per generation', '25'),
+ (10, 2, 'number of offspring per generation', '25'),
+ (11, 2, 'crossover rate', '0.95');
  
 INSERT INTO datareliability (datareldesc) VALUES ('Actual data'),('Technical report'),('Scientific bibliography'),('Expert opinion');
 
