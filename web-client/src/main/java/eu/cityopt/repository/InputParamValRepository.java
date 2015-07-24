cec89772ca13e93f5fb265484c076a79a8c1158c
fnadler <fnadler@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-07-24 10:13:07 +0000

    Sorting extended

--- web-client/src/main/java/eu/cityopt/repository/InputParamValRepository.java
+++ web-client/src/main/java/eu/cityopt/repository/InputParamValRepository.java
@@ -2,6 +2,8 @@ package eu.cityopt.repository;
 
 import java.util.List;
 
+import javax.persistence.OrderBy;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,6 +23,7 @@ public interface InputParamValRepository extends JpaRepository<InputParamVal,Int
 			+ " join ipv.scenario s"
 			+ " where ip.inputid = :inputid "
 			+ " and s.scenid = :scenid ")
+	@OrderBy("ip.name")
 	InputParamVal findByInputIdAndScenId(@Param("inputid") int inputid, @Param("scenid") int scenid);
 
 	@Query("select ipv from InputParamVal ipv "
@@ -29,6 +32,7 @@ public interface InputParamValRepository extends JpaRepository<InputParamVal,Int
 			+ " join ipv.scenario s"
 			+ " where c.componentid = :componentid"
 			+ " and s.scenid = :scenid ")
+	@OrderBy("ip.name")
 	List<InputParamVal> findByComponentAndScenario(@Param("componentid") int componentid, 
 			@Param("scenid") int scenid);
 	
@@ -38,12 +42,14 @@ public interface InputParamValRepository extends JpaRepository<InputParamVal,Int
 			+ " join ipv.scenario s"
 			+ " where c.componentid = :componentid"
 			+ " and s.scenid = :scenid ")
+	@OrderBy("ip.name")
 	Page<InputParamVal> findByComponentAndScenario(@Param("componentid") int componentid, 
 			@Param("scenid") int scenid,Pageable pageable);
 	
 	@Query("select i from InputParamVal i where "
 			+ " Lower(i.inputparameter.name) like Lower(:name)"
 			+ " and i.scenario.scenid = :scenid")
+	@OrderBy("ip.name")
 	InputParamVal findByNameAndScenario(@Param("name") String name, @Param("scenid") int scenId);
 	
 }
