49c1c7298b795b64ef5ca69d24c5f35ce241404a
fnadler <fnadler@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-07-24 07:31:21 +0000

    Schedule cleanup (daily, weekly,...)

--- web-client/src/main/java/eu/cityopt/service/impl/MaintenanceServiceImp.java
+++ web-client/src/main/java/eu/cityopt/service/impl/MaintenanceServiceImp.java
@@ -1,6 +1,7 @@
 package eu.cityopt.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,9 @@ public class MaintenanceServiceImp implements MaintenanceService {
 	
 	@Override
 	@Transactional
+	//@Scheduled(fixedDelay=5000)
+	//@Scheduled(cron="0 0 22 1/1 * ? *")
+	//@Scheduled(cron="0 0 22 ? * SAT *")
 	public void cleanupEntities() {
 		extParamValSetService.cleanupExtParamValSets();		
 	}
