e72512925933e9defecf8662925b2a2eddb80144
mturunen <mturunen@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-11-12 13:03:09 +0000

    I resolved a Controller conflict by rejecting my own changes. and updating my project into date. Then I created my new features  I implemented New Service Project Management Service. That is supposed to handle business logic of the user Management.
    I implemented two Business logic features in use management:
    -When person creates a project it applies the creator the project Administrator role for the creator of the project.
    -Open project opens only the available projects that has permission.

---
+++ web-client/src/main/java/eu/cityopt/service/ProjectManagementService.java
@@ -0,0 +1,9 @@
+package eu.cityopt.service;
+
+import eu.cityopt.DTO.ProjectDTO;
+
+public interface ProjectManagementService {
+
+	public ProjectDTO createProjectWithAdminUser(ProjectDTO NewProject, String user);	
+	
+}
