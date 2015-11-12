10e4d147220ad9e1e847fbade742da62f7d21be2
tkorvola <tkorvola@a1fcc779-133f-4713-a9a5-57ed522004e2>
2015-11-12 17:34:38 +0000

    Have CityoptDistributorModule configure the individual completer.
    CityoptDistributorModule now reads the node config file (at
    configuration time), counts the total number of CPUs specified there,
    and if > 1 installs IndividualCompleterModule and configures it for
    the appropriate number of threads. It follows that you neither need
    nor should attempt to install IndividualCompleterModule yourself
    (e.g., via the GUI) if using CityoptDistributorModule.

--- trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/CityoptDistributorModule.java
+++ trunk/opt-ga/src/main/java/eu/cityopt/opt/ga/CityoptDistributorModule.java
@@ -1,14 +1,18 @@
 package eu.cityopt.opt.ga;
 
-import java.nio.file.Path;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
+import org.opt4j.core.common.completer.IndividualCompleterModule;
 import org.opt4j.core.config.Icons;
 import org.opt4j.core.config.annotations.File;
 import org.opt4j.core.config.annotations.Icon;
 import org.opt4j.core.config.annotations.Info;
 import org.opt4j.core.start.Opt4JModule;
 
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 
 @Icon(Icons.PUZZLE_BLUE)
@@ -17,12 +21,27 @@ public class CityoptDistributorModule extends Opt4JModule {
     @Info("Distributed node configuration")
     @File(".json")
     private String nodeFile = "";
-
+    
     @Override
     protected void config() {
         if (!nodeFile.isEmpty()) {
-            bind(Path.class).annotatedWith(Names.named("nodeConfig"))
-                    .toInstance(Paths.get(nodeFile));
+            try {
+                List<Map<String, String>>
+                    nodes = AprosFactory.readNodeConfig(Paths.get(nodeFile));
+                bind(new TypeLiteral<List<Map<String, String>>>() {})
+                    .annotatedWith(Names.named("nodeConfig"))
+                    .toInstance(nodes);
+                int n = AprosFactory.numCpus(nodes);
+                if (n > 1) {
+                    IndividualCompleterModule
+                        icm = new IndividualCompleterModule();
+                    icm.setType(IndividualCompleterModule.Type.PARALLEL);
+                    icm.setThreads(n);
+                    install(icm);
+                }
+            } catch (IOException e) {
+                addError(e);
+            }
         }
     }
 
