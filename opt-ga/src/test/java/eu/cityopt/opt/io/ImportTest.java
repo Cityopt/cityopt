be6c27d8ba0f46f3977a10283a99457427778be1
Timo Korvola <Timo.Korvola@vtt.fi>
2018-10-03 19:11:15 +0300

    Refactor Apros support into a separate module.

--- opt-ga/src/test/java/eu/cityopt/opt/io/ImportTest.java
+++ opt-ga/src/test/java/eu/cityopt/opt/io/ImportTest.java
@@ -5,17 +5,13 @@ import static org.junit.Assume.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
-import java.util.stream.Collectors;
 
 import org.junit.*;
 import org.junit.rules.ExpectedException;
-import org.opt4j.core.start.Opt4JTask;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
@@ -25,17 +21,12 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.collect.Lists;
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
-import com.google.inject.grapher.graphviz.GraphvizGrapher;
-import com.google.inject.grapher.graphviz.GraphvizModule;
 import com.google.inject.name.Names;
 import com.google.inject.util.Providers;
 
-import eu.cityopt.opt.ga.CityoptDistributorModule;
-import eu.cityopt.opt.ga.CityoptFileModule;
 import eu.cityopt.opt.ga.TimeSeriesLoader;
 import eu.cityopt.sim.eval.EvaluationSetup;
 import eu.cityopt.sim.eval.Evaluator;
@@ -46,40 +37,12 @@ import eu.cityopt.sim.eval.TimeSeriesI;
 import eu.cityopt.sim.eval.Type;
 import eu.cityopt.sim.opt.OptimisationProblem;
 import eu.cityopt.sim.opt.SimulationStructure;
-import eu.cityopt.test.TestResources;
 
-public class ImportTest {
-    private static TestResources res;
-
+public class ImportTest extends TestBase {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    public class TestModule extends AbstractModule {
-        Properties props = res.properties;
-        Instant t0 = Instant.parse(props.getProperty("time_origin"));
-        Path mfile = res.getPath("model_file");
-        Path pfile = res.getPath("problem_file");
-        Path sfile = res.getPath("scenario_file");
-        Path[] tsfiles = res.getPaths("ts_files");
-
-        TestModule() {}
-
-        @Override
-        protected void configure() {
-            bind(Instant.class).annotatedWith(Names.named("timeOrigin"))
-                    .toInstance(t0);
-            bind(Path.class).annotatedWith(Names.named("model"))
-                    .toInstance(mfile);
-            bind(Path.class).annotatedWith(Names.named("problem"))
-                    .toInstance(pfile);
-            bind(Path.class).annotatedWith(Names.named("scenario"))
-                    .toInstance(sfile);
-            bind(Path[].class).annotatedWith(Names.named("timeseries"))
-                    .toInstance(tsfiles);
-        }
-    }
-
-    public class TsTestModule extends TestModule {
+    public static class TsTestModule extends TestModule {
         Evaluator evaluator = new Evaluator();
 
         @Override
@@ -91,11 +54,6 @@ public class ImportTest {
         }
     }
 
-    @BeforeClass
-    public static void setupProps() throws Exception {
-        res = new TestResources();
-    }
-
     @Test
     public void testIsMaximize() {
         String[] cases = {"min", "Min", "MIN", "max", "Max", "MAX", "bork"};
@@ -103,16 +61,6 @@ public class ImportTest {
         List<Boolean> results = Lists.transform(
                 Arrays.asList(cases), JacksonBinder.Obj::isMaximize);
         assertArrayEquals(exptd, results.toArray());
-    }
-
-    public void checkProblem(OptimisationProblem p) {
-        assertTrue(p.inputConst.getExternalParameters().isComplete());
-        assertFalse(p.inputConst.isComplete());
-        assertEquals(1, p.constraints.size());
-        assertEquals(2, p.decisionVars.size());
-        assertEquals(2, p.inputExprs.size());
-        assertEquals(2, p.metrics.size());
-        assertEquals(1, p.objectives.size());
     }
 
     @Test
@@ -286,52 +234,5 @@ public class ImportTest {
                 wtr = inj.getInstance(CsvTimeSeriesWriter.class);
         //wtr.setNumeric(true);
         wtr.write(System.out, tsd);
-    }
-
-    private CityoptFileModule getCityoptFileModule() {
-        TestModule tm = new TestModule();
-        CityoptFileModule cfm = new CityoptFileModule();
-        cfm.setModelFile(tm.mfile.toString());
-        cfm.setProblemFile(tm.pfile.toString());
-        String sep = System.getProperty("path.separator");
-        String p = Arrays.stream(tm.tsfiles)
-                .map(Path::toString).collect(Collectors.joining(sep));
-        cfm.setTimeSeriesFile(p);
-        cfm.setTimeOrigin(tm.t0.toString());
-        return cfm;
-    }
-
-    @Test
-    public void testInjectProblem() throws Exception {
-        CityoptFileModule cfm = getCityoptFileModule();
-        Opt4JTask task = new Opt4JTask(false);
-        try {
-            task.init(cfm);
-            task.open();
-            OptimisationProblem p = task.getInstance(OptimisationProblem.class);
-            checkProblem(p);
-            assertNotNull(p.model);
-        } finally {
-            task.close();
-        }
-    }
-
-    // dot -Tpdf -Grankdir=LR -O cfm-deps.dot
-    @Test
-    public void graphCityoptModules() throws IOException {
-        String dotname = res.properties.getProperty("dot_file");
-        assumeFalse("Output file name (dot_file) not set",
-                    dotname == null || dotname.isEmpty());
-        CityoptDistributorModule distr = new CityoptDistributorModule();
-        CityoptOutputModule output = new CityoptOutputModule();
-        Injector cf_inj = Guice.createInjector(
-                getCityoptFileModule(), distr, output);
-        Injector gv_inj = Guice.createInjector(new GraphvizModule());
-        GraphvizGrapher g = gv_inj.getInstance(GraphvizGrapher.class);
-        try (PrintWriter out = new PrintWriter(dotname)) {
-            System.out.println("Writing graph to " + dotname);
-            g.setOut(out);
-            g.graph(cf_inj);
-        }
     }
 }
